5a052528b8353a282d60e8aae8e59eeb4eb0b23b
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2025-02-10 09:48:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,41 +7,32 @@ terraform {
   }
 }
 
-# Configure the IBM Provider
 provider "ibm" {
   region = "eu-es"
   ibmcloud_api_key=var.api_key
 }
 
-
-
-resource "ibm_is_vpc" "vpc_module_abermudez" {
-  name = "vpc-abermudez"
+resource "ibm_is_vpc" "vpc_module_rgonzalez" {
+  name = "vpc-rgonzalez"
   resource_group = var.resource_group
-
 }
 
-resource "ibm_is_subnet" "subnet_module_abermudez" {
-  name = "subnet-abermudez"
-  vpc = ibm_is_vpc.vpc_module_abermudez.id
+resource "ibm_is_subnet" "subnet_module_rgonzalez" {
+  name = "subnet-rgonzalez"
+  vpc = ibm_is_vpc.vpc_module_rgonzalez.id
   zone = "eu-es-1"
   ipv4_cidr_block = "10.251.10.0/24"
   resource_group  = var.resource_group  
-
-
 }
 
-
-resource "ibm_is_security_group" "ssh_abermudez_security_group" {
+resource "ibm_is_security_group" "ssh_rgonzalez_security_group" {
   name            = "ssh-security-group"
-  vpc          =  ibm_is_vpc.vpc_module_abermudez.id
+  vpc          =  ibm_is_vpc.vpc_module_rgonzalez.id
   resource_group  = var.resource_group  
 }
 
-
-
 resource "ibm_is_security_group_rule" "ssh_rule" {
-  group     = ibm_is_security_group.ssh_abermudez_security_group.id
+  group     = ibm_is_security_group.ssh_rgonzalez_security_group.id
   direction = "inbound"
   remote    = "0.0.0.0/0"
   tcp {
@@ -50,38 +41,32 @@ resource "ibm_is_security_group_rule" "ssh_rule" {
   }
 }
 
-
-
-resource "ibm_is_ssh_key" "ssh_key_abermudez" {
-  name       = "ssh-key-abermudez"
+resource "ibm_is_ssh_key" "ssh_key_rgonzalez" {
+  name       = "ssh-key-rgonzalez"
   public_key = <<-EOF
- ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDQYePYr1IxSOGxJ6+lKuD4onsLK8jxU93BvYAB2lxTgomteXCpdHnKK3jix8hxadmANkG/k9kEjxWwKQR7ZVyw8eQul3aLCfMnHGqplVQH3JSsz5bKMaCNx8r2P5SYGLeTmbixZUmjlFxeacEQ7/8RPvVESZ5IvrOpNtsW0kF3IsxXZndLhZlC+a69xIw2UTDVYRjwSFcB4BLl2Z3YPIwcFNWyDQdThmSWJkfdXxOmunaVRVK+OFhEAJmIf8TJ6JVBbsBf1RU2khD8M3zGpxTKF6W0rb9seEkfHERhJbYpv8NmyWST8vgyCYRElKQK+IWmT4qMua+q6eXcrUtalyZa1m8rIytze10sa4kBsN/fdr/rtACDo+hx/e1lU5GnwodPscFaVHHH5nIOF1iq4llRevoPsTvSwViAE9Se1BrLZC1MrpyxF8l7LTDqCYbRuWoTXP5w5ElbqKIEbaBvv3xhd8V7jW0VYvg/vSbD9ZApAmb7QRnzzjGLCKS9k5/rOvhtcT/FP7XXxivnc+tRp7Q+FRjHAPgmhd9unk/LTUjXhaD9+M30nDol39jT+jwBZ8JOW1rFEFQJkGM7wfqSzbJRQutH5VMCX3XSk1+qv2hz5Sza1IJJPfeleetFRT9b1AbU/TCRpOg7ZwrcvMd9xyWFacHTqaUR2/oXF2c6FzT6FQ== abermudez@stemdo
+  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCU94A3wzNYKAAYrOgQ6OGPcLVNYb73+FF5r/Vp/upSghDbdRzW95xm4BBTqaR+8Dm81UFycPjJlYnUaKYlrjGpTxKLoX6myC/RA0ddYH9WAD6ZRqdXepELdoikiZyvMOaMgOT5t6t9z9tWCuzkgvc5L8goYfHXzP44iGrkqR3Vf0Q3PmnHedFFFShbcT3p1vKR/9Z7VFF2my0Weg0C7tpE7VRBQ1dFlhzKCbAhWQ9SqZUowlh7/ASGzgX9K9czV6MtvE932YudPlSKrpD1GRejY+sndAfl1yOObyvKkUXmMjoqWIsRV3QBJtTNJNQk09MHMmwNEvTlW7T+ffe3Asqz
   EOF
   resource_group = var.resource_group
-  
 }
 
-
-resource "ibm_is_instance" "vm_abermudez" {
-  name              = "vm-abermudez"
-  vpc               = ibm_is_vpc.vpc_module_abermudez.id
+resource "ibm_is_instance" "vm_rgonzalez" {
+  name              = "vm-rgonzalez"
+  vpc               = ibm_is_vpc.vpc_module_rgonzalez.id
   profile           = "bx2-2x8"
   zone              = "eu-es-1"
-  keys = [ibm_is_ssh_key.ssh_key_abermudez.id]  
+  keys = [ibm_is_ssh_key.ssh_key_rgonzalez.id]
   image             = "r050-b98611da-e7d8-44db-8c42-2795071eec24"
   resource_group = var.resource_group
 
   primary_network_interface {
-    subnet          = ibm_is_subnet.subnet_module_abermudez.id
-    security_groups = [ibm_is_security_group.ssh_abermudez_security_group.id]
-
+    subnet          = ibm_is_subnet.subnet_module_rgonzalez.id
+    security_groups = [ibm_is_security_group.ssh_rgonzalez_security_group.id]
   }
 }
 
 resource "ibm_is_floating_ip" "public_ip" {
-  name   = "public-ip-abermudez"
-  target = ibm_is_instance.vm_abermudez.primary_network_interface[0].id
+  name   = "public-ip-rgonzalez"
+  target = ibm_is_instance.vm_rgonzalez.primary_network_interface[0].id
   resource_group = var.resource_group
-  depends_on = [ibm_is_instance.vm_abermudez]
-
+  depends_on = [ibm_is_instance.vm_rgonzalez]
 }
