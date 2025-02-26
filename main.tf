9236109094eec689558b4de217bc2b69a87c9e2b
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2025-02-26 11:41:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,10 +101,11 @@ resource "ibm_resource_instance" "cos_instance" {
 # }
 
 resource "ibm_iam_user_policy" "usuario_policy" {
-  iam_id = "acajas@stemdo.io"
+  ibm_id = "acajas@stemdo.io"
   roles  = ["Reader", "Writer", "Manager", "Viewer"] 
 
   resources {
+    service = "containers-kubernetes"
     resource_type = "cluster"
     resource      = ibm_container_vpc_cluster.cluster.id
   }
