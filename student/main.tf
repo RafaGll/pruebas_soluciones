bcf7843f6eee53871d4f386a6b9c12eccf5794b4
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2025-03-03 16:24:18 +0100

    Update main.tf

--- student/main.tf
+++ student/main.tf
@@ -20,7 +20,7 @@ data "ibm_iam_access_group" "wiki" {
   access_group_name = "Stemdo_TEST"
 }
 
-resource "ibm_iam_access_group_policy" "group_policy_viewer" {
+resource "ibm_iam_access_group_policy" "containers" {
   depends_on = [ data.ibm_iam_access_group.wiki ]
   access_group_id = data.ibm_iam_access_group.wiki.groups[0].id
   roles           = ["Viewer"] 
@@ -30,8 +30,8 @@ resource "ibm_iam_access_group_policy" "group_policy_viewer" {
   }
 }
 
-resource "ibm_iam_access_group_policy" "group_policy_viewer" {
-  depends_on = [ data.ibm_iam_access_group.wiki ]
+resource "ibm_iam_access_group_policy" "vpc" {
+  depends_on = [ ibm_iam_access_group_policy.containers ]
   access_group_id = data.ibm_iam_access_group.wiki.groups[0].id
   roles           = ["Viewer"] 
   resource_attributes {
